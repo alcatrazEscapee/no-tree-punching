4021f1d9876b03e932319bb14086d0f84aa66159
alcatrazEscapee <alex@molleroneill.com>
2018-02-19 19:08:19 -0500

    Removed build from cache

--- build/sources/main/java/notreepunching/proxy/ServerProxy.java
+++
@@ -1,8 +0,0 @@
-package notreepunching.proxy;
-
-import net.minecraftforge.fml.common.Mod;
-import notreepunching.proxy.ClientProxy;
-
-@Mod.EventBusSubscriber
-public class ServerProxy extends ClientProxy {
-}
