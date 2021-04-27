507707c0602241de8485ad0262ea075e67bac683
AlcatrazEscapee <alex@molleroneill.com>
2021-04-27 12:52:05 -0400

    License check and gradle plugin.

--- src/test/java/com/alcatrazescapee/notreepunching/HarvestBlockHandlerTests.java
+++ src/test/java/com/alcatrazescapee/notreepunching/HarvestBlockHandlerTests.java
@@ -1,3 +1,8 @@
+/*
+ * Part of the No Tree Punching mod by AlcatrazEscapee.
+ * Work under copyright. See the project LICENSE.md for details.
+ */
+
 package com.alcatrazescapee.notreepunching;
 
 import net.minecraft.block.Block;
